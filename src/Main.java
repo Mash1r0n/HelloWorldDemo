1ee0868d91d0d1f52b1c0f6473ac236c51a66fe1
Stanislav <stasmolchanov7@gmail.com>
2024-09-10 09:40:37 +0300

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.time.LocalDateTime;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String User = "Stanislav Molchanov";
-        System.out.printf("This task done by: " + User);
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.printf("This task done by: " + User + " | " + dateTime.toString());
     }
 }
