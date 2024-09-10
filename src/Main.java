288abd1356489f941b523f18d493c7a242de3e52
Stanislav <stasmolchanov7@gmail.com>
2024-09-10 09:30:13 +0300

    Start

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String User = "Molchanov Stanislav";
+        System.out.printf("This task done by: " + User);
+    }
+}
